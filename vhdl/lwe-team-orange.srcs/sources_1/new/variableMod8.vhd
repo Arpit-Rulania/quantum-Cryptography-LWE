214a5aba0da7cb7fb2189ffd5d25821c1336cd86
Andrew Wong <andrew.j.wong@outlook.com>
2021-10-16 04:14:00 +1100

    format mod

--- vhdl/lwe-team-orange.srcs/sources_1/new/variableMod8.vhd
+++ vhdl/lwe-team-orange.srcs/sources_1/new/variableMod8.vhd
@@ -1,109 +1,80 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 04.10.2021 00:43:10
--- Design Name: 
--- Module Name: variableMod8 - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.NUMERIC_STD.ALL;
 
+ENTITY variableMod8 IS
+    PORT (
+        clk : IN STD_LOGIC;
+        rst : IN STD_LOGIC;
+        inQ : IN STD_LOGIC_VECTOR (7 DOWNTO 0);
+        input : IN STD_LOGIC_VECTOR (7 DOWNTO 0);
+        output : OUT STD_LOGIC_VECTOR (7 DOWNTO 0);
+        ready : OUT STD_LOGIC
+    );
+END variableMod8;
 
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.NUMERIC_STD.ALL;
+ARCHITECTURE Behavioral OF variableMod8 IS
+    SIGNAL intermediate : unsigned(7 DOWNTO 0);
+    SIGNAL q_out : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');
+    SIGNAL isReady : STD_LOGIC := '0';
 
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
-entity variableMod8 is
-    Port ( clk : in std_logic; 
-           rst : in std_logic;
-           inQ : in STD_LOGIC_VECTOR (7 downto 0);
-           input : in STD_LOGIC_VECTOR (7 downto 0);
-           output : out STD_LOGIC_VECTOR (7 downto 0);
-           ready : out std_logic
-       );
-end variableMod8;
-
-architecture Behavioral of variableMod8 is
-    signal intermediate : unsigned(7 downto 0);
-    signal q_out : std_logic_vector(7 downto 0) := (others => '0');
-    signal isReady : std_logic := '0';
-    
-    signal scaled_q : unsigned(7 downto 0);
-    signal hasScaled : boolean;
-begin
+    SIGNAL scaled_q : unsigned(7 DOWNTO 0);
+    SIGNAL hasScaled : BOOLEAN;
+BEGIN
     output <= q_out;
     ready <= isReady;
-    
-    process(clk)
-    begin
-    
-        if rising_edge(clk) then
-            if rst = '1' then
+
+    PROCESS (clk)
+    BEGIN
+
+        IF rising_edge(clk) THEN
+            IF rst = '1' THEN
                 scaled_q <= unsigned(inQ);
                 intermediate <= unsigned(input);
-                q_out <= (others => '0');
+                q_out <= (OTHERS => '0');
                 isReady <= '0';
                 hasScaled <= false;
-            elsif isReady = '0' then
-             
+            ELSIF isReady = '0' THEN
+
                 -- OPTIMISATION - https://math.stackexchange.com/questions/3559467/how-is-calculating-the-modulus-using-this-formula-faster
-                
+
                 -- if mod 0
                 ---- unfortunate.
-                
-                
                 -- TODO: SHOULD THESE GO INTO THE RESET LOOP?
                 -- Unlikely that we'll get these
-                if unsigned(inQ) = 1 then
+                IF unsigned(inQ) = 1 THEN
                     -- X mod 1 = 0
-                    q_out <= (others => '0');
+                    q_out <= (OTHERS => '0');
                     isReady <= '1';
-                elsif unsigned(inQ) = 2 then
+                ELSIF unsigned(inQ) = 2 THEN
                     -- X mod 2 = LSB
-                    q_out <= (0 => input(0), others => '0');
+                    q_out <= (0 => input(0), OTHERS => '0');
                     isReady <= '1';
-                else
-                
-                
-                if hasScaled = false and scaled_q < intermediate then
+                ELSE
+                    IF hasScaled = false AND scaled_q < intermediate THEN
                         -- Left shift, multiply by two, arrange the bits
                         -- idk
-                        scaled_q <= scaled_q(6 downto 0) & '0';
-                    else 
+                        scaled_q <= scaled_q(6 DOWNTO 0) & '0';
+                    ELSE
                         hasScaled <= true;
-    
-                        if intermediate < unsigned(inQ) then
-                            q_out <= std_logic_vector(intermediate);
+
+                        IF intermediate < unsigned(inQ) THEN
+                            q_out <= STD_LOGIC_VECTOR(intermediate);
                             isReady <= '1';
-                        else
+                        ELSE
                             -- Subtract multiples of Q
-                            if scaled_q <= intermediate then
+                            IF scaled_q <= intermediate THEN
                                 -- The exit condition for hasScaled causes scaled_q >= intermediate
                                 -- Ensure we only subtract if scaled_q intermediate
                                 intermediate <= intermediate - scaled_q;
-                            end if;
-                            
-                            scaled_q <= '0' & scaled_q(7 downto 1);                        
-                        end if;
-                    end if;
-                end if;
-            end if;
-        end if;
-    end process;
+                            END IF;
 
-end Behavioral;
+                            scaled_q <= '0' & scaled_q(7 DOWNTO 1);
+                        END IF;
+                    END IF;
+                END IF;
+            END IF;
+        END IF;
+    END PROCESS;
+
+END Behavioral;
