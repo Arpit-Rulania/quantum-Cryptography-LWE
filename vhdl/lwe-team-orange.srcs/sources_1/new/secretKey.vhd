1747fa5477ad12c8c99c7014d8487e39921e1a9e
Andrew Nicholson <82710850+andrewnich@users.noreply.github.com>
2021-10-16 17:07:46 +1100

    Secret Key Generation

---
+++ vhdl/lwe-team-orange.srcs/sources_1/new/secretKey.vhd
@@ -0,0 +1,66 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 10/05/2021 12:55:16 PM
+-- Design Name: 
+-- Module Name: rngnum - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+-- i = number of columns in A matrix
+entity secretKey is
+  Generic ( i : integer );
+  PORT (Clk, Rst: IN std_logic;
+    secret: OUT array (0 to i-1) of std_logic_vector (15 downto 0);
+    ready: OUT std_logic);
+end secretKey;
+
+architecture Behavioral of secretKey is
+  component rngnum is
+    PORT (Clk, Rst: IN std_logic;
+      output: OUT std_logic_vector (15 DOWNTO 0));
+  end component;  
+  
+  SIGNAL a: std_logic_vector (15 DOWNTO 0);
+  SIGNAL k: integer:= 0;
+  SIGNAL isReady: std_logic:= '0';
+    
+begin
+  
+  ready <= isReady;
+  Stage0: rngnum PORT MAP (Clk, Rst, a);
+  getRand : process(a, k)
+  begin
+    if k < i then
+      secret(k) <= a;
+      k <= k + 1;
+    elsif k = i then
+      isReady <= 1;
+    end if;
+  end process getRand;
+
+end Behavioral;
