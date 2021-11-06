68529051c0c38e9d8c9619234a0813271a14ccd5
Andrew Wong <andrew.j.wong@outlook.com>
2021-11-06 23:43:49 +1100

    Fixup secret key testbench

--- vhdl/lwe-team-orange.srcs/sim_1/new/secretKey_tb.vhd
+++ vhdl/lwe-team-orange.srcs/sim_1/new/secretKey_tb.vhd
@@ -1,85 +1,110 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 11/02/2021 12:36:39 AM
--- Design Name: 
--- Module Name: seck_tb - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- ----
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+use work.commons.all;
 
-package pkg is
-  type t_array is array (natural range <>) of std_logic_vector(15 downto 0);
-end package;
+entity secretKey_TB is
+end secretKey_TB;
 
-package body pkg is
-end package body;
-
-library ieee;
-use ieee.std_logic_1164.all;
-library work;
-use work.pkg.all;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
-entity seck_tb is
-end seck_tb;
-
-architecture Behavioral of seck_tb is
-    component secretKey is
-        Generic ( i : integer );
-        PORT (Clk, Rst: IN std_logic;
-            secret: OUT t_array (0 to i-1);
-            ready: OUT std_logic);
-    end component;
-    SIGNAL Clk_s, Rst_s: std_logic;
-    SIGNAL output_s: std_logic_vector(15 DOWNTO 0);
-    --SIGNAL poutput_s: std_logic_vector(15 DOWNTO 0);
-    SIGNAL s_out: t_array (0 to 15);
-    SIGNAL t_ready: std_logic;
+architecture Behavioral of secretKey_TB is
+    SIGNAL clk: std_logic;
+    SIGNAL rst: std_logic;
     
+    SIGNAL ready: std_logic;
+    SIGNAL secret: t_array (0 to 15);
+    
+    
+    SIGNAL validRng : std_logic := '0';
+    signal randomNum:  std_logic_vector (15 DOWNTO 0);
+        
 begin
-    SecretToTest: secretKey 
-        Generic MAP (i => 16)
-        PORT MAP (Clk_s, Rst_s, s_out, t_ready);
+    SecretToTest: entity work.secretKey 
+        Generic MAP (
+            i => 16
+        )
+        PORT MAP (
+            clk => clk,
+            rst => rst,
+            randomNum => randomNum,
+            validRng => validRng,
+            secret => secret,
+            ready => ready
+       );
 
-    Clk_proc: PROCESS
-    BEGIN
-        Clk_s <= '1';
-        WAIT FOR 10 ns;
-        Clk_s <= '0';
-        WAIT FOR 10 ns;
-    END PROCESS clk_proc;
-                      
+    c: entity work.ClockProvider PORT MAP ( clk => clk );
+    
+    -- 'random' "readyness" generator
+    rng_stub1: process begin
+
+        wait until rising_edge(clk);
+        validRng <= '0';
+
+        wait until rising_edge(clk);
+        validRng <= '0';
+
+        wait until rising_edge(clk);
+        validRng <= '1';
+
+        wait until rising_edge(clk);
+        validRng <= '0';
+
+        wait until rising_edge(clk);
+        validRng <= '1';
+
+        wait until rising_edge(clk);
+        validRng <= '1';
+
+        wait until rising_edge(clk);
+        validRng <= '0';
+
+        wait until rising_edge(clk);
+        validRng <= '0';
+
+        wait until rising_edge(clk);
+        validRng <= '0'; 
+               
+        wait until rising_edge(clk);
+        validRng <= '0';
+
+        wait until rising_edge(clk);
+        validRng <= '0'; 
+               
+
+        wait until rising_edge(clk);
+        validRng <= '1';
+    end process;
+
+    -- Pseudo-random generator
+    rng_stub2: process begin
+        wait until rising_edge(clk);
+        randomNum <= std_logic_vector(to_unsigned(25723, randomNum'length));
+
+        wait until rising_edge(clk);
+        randomNum <= std_logic_vector(to_unsigned(38236, randomNum'length));
+
+        wait until rising_edge(clk);
+        randomNum <= std_logic_vector(to_unsigned(26337, randomNum'length));
+        
+        wait until rising_edge(clk);
+        randomNum <= std_logic_vector(to_unsigned(1142, randomNum'length));
+
+        wait until rising_edge(clk);
+        randomNum <= std_logic_vector(to_unsigned(13396, randomNum'length));
+        
+        wait until rising_edge(clk);
+        randomNum <= std_logic_vector(to_unsigned(50252, randomNum'length));
+
+        wait until rising_edge(clk);
+        randomNum <= std_logic_vector(to_unsigned(64594, randomNum'length));
+    end process;
+
     Vector_proc: PROCESS
     BEGIN
-        Rst_s <= '1';
-        WAIT FOR 5 NS;
-        Rst_s <= '0';
-        FOR index IN 0 To 4 LOOP
-            WAIT UNTIL Clk_s='1' AND Clk_s'EVENT;
-        END LOOP;
-        WAIT FOR 5 NS;
-        ASSERT output_s = X"88" REPORT "Failed output=88";
+        rst <= '1';
+        WAIT FOR 20 NS;
+        rst <= '0';
+        
+        WAIT UNTIL ready = '1';
         WAIT;
     END PROCESS Vector_proc;
 
