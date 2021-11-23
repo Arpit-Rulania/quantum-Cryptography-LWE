339c6035e8f770a1155efcd345538b046e02ba2e
Andrew Wong <andrew.j.wong@outlook.com>
2021-11-23 22:38:14 +1100

    Conform clk, rst and ready signals

--- vhdl/lwe-team-orange.srcs/sources_1/new/encrypt.vhd
+++ vhdl/lwe-team-orange.srcs/sources_1/new/encrypt.vhd
@@ -1,44 +1,34 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
---use ieee.numeric_std_unsigned.all;
 USE IEEE.NUMERIC_STD.ALL;
-library work;
+
 use work.commons.all;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity encrypt is
-    generic (A_width : integer:=16);
-    Port ( Clock : in std_logic;
-           Reset : in std_logic;
+    generic (
+        A_width : integer := 16
+    );
+    Port ( clk : in std_logic;
+           rst : in std_logic;
            M : in std_logic; --bit to be encrypted.
            inQ : in std_logic_vector(15 downto 0);
            sumA : in t_array;
            sumB : in std_logic_vector(15 downto 0);
            u : out t_array;
            v : out std_logic_vector(15 downto 0);
-           encrypt_ready : out std_logic);
+           ready : out std_logic);
 end encrypt;
 
 architecture Behavioral of encrypt is
     signal modInput : std_logic_vector(15 downto 0);
     signal modOutput : std_logic_vector(15 downto 0);
     signal modReady : std_logic;
-    signal modReset : std_logic;
+    signal modrst : std_logic;
     signal genReady : std_logic_vector(0 to A_width-1);
     signal qbytwo : std_logic_vector(15 downto 0) := (others => '0');
     signal sumBfiltered : std_logic_vector(15 downto 0);
-    signal outrdy : std_logic;
 
 begin
-    encrypt_ready <= outrdy;
 
     GEN_MOD:
     for n in 0 to A_width-1 generate
@@ -47,8 +37,8 @@ begin
                 i => 16
             )
             PORT MAP(
-                clk => Clock,
-                rst => Reset,
+                clk => clk,
+                rst => rst,
                 inQ => inQ,
                 input => sumA(n),
                 output => u(n),
@@ -61,19 +51,19 @@ begin
             i => 16
         )
         PORT MAP(
-            clk => Clock,
-            rst => Reset,
+            clk => clk,
+            rst => rst,
             inQ => inQ,
             input => modInput,
             output => v,
             ready => modReady
         );
     
-    encrypt : process(Clock)
+    encrypt : process(clk)
     begin
-        if rising_edge(Clock) then
-            if Reset = '1' then
-                outrdy <= '0';
+        if rising_edge(clk) then
+            if rst = '1' then
+                ready <= '0';
                 if M = '1' then
                     qbytwo <= '0' & inQ(15 downto 1);   -- Q/2
                     --modInput <= sumB - qbytwo;              
@@ -83,7 +73,7 @@ begin
                     modInput <= sumB;
                 end if;
             elsif modReady = '1' and genReady(A_width - 1) = '1' then
-                outrdy <= '1';
+                ready <= '1';
             end if;
         end if;
     end process;
