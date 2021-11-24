cf2248f697b03a31e5cfda2faa7a76ea85695e7d
Arpit-Rulania <91777055+Arpit-Rulania@users.noreply.github.com>
2021-11-25 02:16:30 +1100

    optimised lut usage

--- vhdl/lwe-team-orange.srcs/sources_1/new/encrypt.vhd
+++ vhdl/lwe-team-orange.srcs/sources_1/new/encrypt.vhd
@@ -1,50 +1,50 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+--use ieee.numeric_std_unsigned.all;
 USE IEEE.NUMERIC_STD.ALL;
-
+library work;
 use work.commons.all;
 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
 entity encrypt is
-    generic (
-        A_width : integer := 16
-    );
+    generic (A_width : integer:=16);
     Port ( clk : in std_logic;
            rst : in std_logic;
-           M : in std_logic; --bit to be encrypted.
+           inM : in std_logic; --bit to be encrypted.
            inQ : in std_logic_vector(15 downto 0);
-           sumA : in t_array;
-           sumB : in std_logic_vector(15 downto 0);
-           u : out t_array;
-           v : out std_logic_vector(15 downto 0);
+           inA : in t_array;
+           inB : in std_logic_vector(15 downto 0);
+           outU : out t_array;
+           outV : out std_logic_vector(15 downto 0);
            ready : out std_logic);
 end encrypt;
 
 architecture Behavioral of encrypt is
     signal modInput : std_logic_vector(15 downto 0);
-    signal modOutput : std_logic_vector(15 downto 0);
     signal modReady : std_logic;
-    signal modrst : std_logic;
-    signal genReady : std_logic_vector(0 to A_width-1);
     signal qbytwo : std_logic_vector(15 downto 0) := (others => '0');
-    signal sumBfiltered : std_logic_vector(15 downto 0);
+    signal outrdy : std_logic;
+    signal vCalc : std_logic;
+    signal tempModRst : std_logic;
+    signal tempoutVal : std_logic_vector(15 downto 0);
+    signal counter : integer := 0; 
+    signal uArrt : t_array;
+    signal incSig : std_logic := '1';
+    type StateType is (
+        a,b,c,d,e,f,g
+    );
+    signal State : StateType;
 
 begin
-
-    GEN_MOD:
-    for n in 0 to A_width-1 generate
-        ModuloComponent : ENTITY work.variableMod
-            GENERIC MAP (
-                i => 16
-            )
-            PORT MAP(
-                clk => clk,
-                rst => rst,
-                inQ => inQ,
-                input => sumA(n),
-                output => u(n),
-                ready => genReady(n)
-            );
-    end generate GEN_MOD;        
+    ready <= outrdy;       
         
     ModVComponent : ENTITY work.variableMod
         GENERIC MAP (
@@ -52,10 +52,10 @@ begin
         )
         PORT MAP(
             clk => clk,
-            rst => rst,
+            rst => tempModRst,
             inQ => inQ,
             input => modInput,
-            output => v,
+            output => tempoutVal,
             ready => modReady
         );
     
@@ -63,17 +63,60 @@ begin
     begin
         if rising_edge(clk) then
             if rst = '1' then
-                ready <= '0';
-                if M = '1' then
-                    qbytwo <= '0' & inQ(15 downto 1);   -- Q/2
-                    --modInput <= sumB - qbytwo;              
-                    --modInput <= std_logic_vector(unsigned(sumB(15 downto 0)) - unsigned(qbytwo(15 downto 0)));
-                    modInput <= std_logic_vector(unsigned(sumB(15 downto 0)) - unsigned(qbytwo(15 downto 0)));
-                elsif M = '0' then
-                    modInput <= sumB;
+                tempModRst <= '1';
+                outrdy <= '0';
+                if inM = '1' then
+                    modInput <= std_logic_vector(unsigned(inB(15 downto 0)) - unsigned('0' & inQ(15 downto 1)));
+                elsif inM = '0' then
+                    modInput <= inB;
                 end if;
-            elsif modReady = '1' and genReady(A_width - 1) = '1' then
-                ready <= '1';
+                vCalc <= '1';
+            end if;
+            if rst = '0' and vCalc = '1' then
+                tempModRst <= '0';
+                if modReady = '1' then
+                    outV <= tempoutVal;
+                    modInput <= inA(0);
+                    vCalc <= '0';
+                end if;
+            end if;
+            if rst = '0' and vCalc = '0' then
+                case State is 
+                    When a =>
+                        if counter < A_width then
+                            State <= b;
+                        else 
+                            State <= g;
+                        end if;
+                    When b =>
+                        if modReady = '0' then
+                            State <= c;
+                        else 
+                            State <= d; 
+                        end if;
+                    When c =>
+                        tempModRst <= '0';
+                        if modReady = '1' then
+                            State <= d;
+                        end if;
+                    When d =>
+                        uArrt(counter) <= tempoutVal;
+--                        if counter < A_width then
+--                            modInput <= inA(counter + 1);
+--                        end if;
+                        State <= e;
+                    When e =>
+                        tempModRst <= '1';
+                        State <= f;
+                    When f =>
+                        counter <= counter + 1;
+                        modInput <= inA(counter);
+                        State <= a;
+                    When g =>
+                        outU <= uArrt;
+                        outrdy <= '1'; 
+                end case;
+            
             end if;
         end if;
     end process;
