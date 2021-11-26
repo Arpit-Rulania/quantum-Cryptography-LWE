14065aaea670b2dc11866463b53c7d445af9ce01
daryllzy <47728071+daryllzy@users.noreply.github.com>
2021-11-26 13:42:52 +0800

    Change signals to align with multiply.vhd

--- vhdl/lwe-team-orange.srcs/sim_1/new/ApproxMulTB.vhd
+++ vhdl/lwe-team-orange.srcs/sim_1/new/ApproxMulTB.vhd
@@ -9,34 +9,35 @@ end ApproxMulTB;
 architecture Behavioral of ApproxMulTB is
 
 constant clkPeriod  : time := 100 ns;
-------------------------------
+
 signal r_clk   : std_logic := '0';
 signal r_reset : std_logic := '0';
 signal result : std_logic_vector(31 downto 0);
-------------------------------
+signal ready : std_logic;
+
 component ApproxMul is
     port(
         clk : in std_logic;
         rst : in std_logic;
-        approxmul_in_a    : in std_logic_vector(15 downto 0);
-        approxmul_in_b    : in std_logic_vector(15 downto 0);
-        approxmul_result  : out std_logic_vector(31 downto 0)
+        in1    : in std_logic_vector(15 downto 0);
+        in2    : in std_logic_vector(15 downto 0);
+        output  : out std_logic_vector(31 downto 0);
+        ready : out std_logic
         );
 end component;
-signal a : std_logic_vector(15 downto 0):="0000000111111111";
-signal b : std_logic_vector(15 downto 0):="0000000000110101";
+signal a : std_logic_vector(15 downto 0):="0000000001000010";
+signal b : std_logic_vector(15 downto 0):="0000000000001011";
 
 begin
    
-    
-    -- Instantiate the Unit Under Test (UUT)
     UUT: ApproxMul
     port map(
         clk => r_clk,
         rst => r_reset,
-        approxmul_in_a => a,
-        approxmul_in_b => b,
-        approxmul_result => result
+        in1 => a,
+        in2 => b,
+        output => result,
+        ready => ready
             );
     
     p_CLK_GEN : 
